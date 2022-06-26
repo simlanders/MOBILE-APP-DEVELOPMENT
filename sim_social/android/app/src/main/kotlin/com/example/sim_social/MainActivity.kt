5a47466ec96e69a114dbd55ca008161284e0331e
simlanders <simeonlanders2022.cs.grad@gmail.com>
2022-06-25 23:44:25 -0400

    Setting Things Up

--- sim_social/android/app/src/main/kotlin/com/example/sim_social/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sim_social
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ simsocial/android/app/src/main/kotlin/com/example/simsocial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simsocial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
